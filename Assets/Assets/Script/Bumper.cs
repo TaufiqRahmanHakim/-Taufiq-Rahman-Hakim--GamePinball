7f6970c6bf86fa998e1ade38206544d30953d629
Taufiq Rahman Hakim <pxpiksy@gmail.com>
2024-09-11 18:01:07 +0700

    SoundManagerFix

--- Assets/Assets/Script/Bumper.cs
+++ Assets/Assets/Script/Bumper.cs
@@ -26,7 +26,7 @@ public class Bumper : MonoBehaviour
         {
             GetComponent<MeshRenderer>().material = materialHited;
             animator.SetBool("Play", true);
-            SoundManager.instance.playBumperAudio();
+            SoundManager.instance.PlaySFX(collision.transform.position);
             Debug.Log("test");
             // ambil rigidbody nya lalu kali kecepatannya sebanyak multiplier agar bisa memantul lebih cepat
             Rigidbody bolaRig = bola.GetComponent<Rigidbody>();
--- Assets/Assets/Script/Manager/SoundManager.cs
+++ Assets/Assets/Script/Manager/SoundManager.cs
@@ -7,20 +7,23 @@ public class SoundManager : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
 
     [SerializeField] private List<AudioClip> clipList;
+
+    public GameObject sfxAudioSource;
     public static SoundManager instance { get; private set; }
     private void Awake()
     {
         instance = this;
     }
-
-    public void playBumperAudio()
+    public void PlaySFX(Vector3 spawnPosition)
     {
-        audioSource.clip = clipList[0];
-        audioSource.Play();
+        // berbeda dengan bgm, disini kita buat script untuk 
+        // memunculkan prefabnya pada posisi sesuai dengan parameternya
+        GameObject.Instantiate(sfxAudioSource, spawnPosition, Quaternion.identity);
     }
+
     public void playPaddleAudio()
     {
-        audioSource.clip = clipList[1];
+        audioSource.clip = clipList[0];
         audioSource.Play();
     }
 }
